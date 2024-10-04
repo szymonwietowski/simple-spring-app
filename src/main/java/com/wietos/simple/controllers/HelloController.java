c9d22d4496891b4ea936601488bb8ff4466be195
szymonwietowski <szymon1402@wp.pl>
2024-10-04 10:00:27 +0200

    controller

--- src/main/java/com/wietos/simple/controllers/HelloController.java
+++ src/main/java/com/wietos/simple/controllers/HelloController.java
@@ -1,4 +1,18 @@
 package com.wietos.simple.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello " + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+    }
 }
