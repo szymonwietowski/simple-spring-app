e8c7b441eb07931085e57c5915ea3562e7c4d57e
szymonwietowski <szymon1402@wp.pl>
2024-10-05 18:33:49 +0200

    jsp form

--- src/main/java/com/wietos/simple/controllers/HelloController.java
+++ src/main/java/com/wietos/simple/controllers/HelloController.java
@@ -1,18 +1,30 @@
 package com.wietos.simple.controllers;
 
+import com.wietos.simple.model.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
-    public String hello() {
-        return "Hello " + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+    public String hello(Model model) {
+        model.addAttribute("message", "my mesasge");
+        model.addAttribute("tasks", List.of("task1", "task2"));
+        model.addAttribute("greeting", new Greeting());
+        return "hello";
+    }
+
+    @PostMapping("/greeting")
+    public String greeting(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("text", greeting.getContent());
+        return "greeting";
     }
 }
